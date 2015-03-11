f50f4b5ff6ef0a36a8577a59c7b9e81a9c10272b
zulldesign <admin@zulldesign.ml>
2015-03-11 19:25:30 +0800

    l

---
+++ BengkelPokokEmas/Form1.php
@@ -0,0 +1,28 @@
+<?php
+// This is designer-generated file.
+// Do not put your code in this file or it can be destroyed by designer.
+// Put all your code, move all your event handlers to Form1.user.php.
+namespace BengkelPokokEmas{
+	partial class Form1 extends \System\Windows\Forms\Form{
+		/// <summary>Constructor creates new instance of the <see cref="Form1"/> class</summary>
+	    public function __construct(/*you can add parameters if you want*/){
+	        $this->InitializeComponent(); //Do not remove this line, it's requred by desiner.
+	        //Uncomment following line and add the Initialize function to Form1.user.php to provide any custom initialization.
+	        //$this->Initialize();
+	    }
+		/// <summary>Required method for Designer support - do not modify the contents of this method with the code editor.</summary>
+		function InitializeComponent(){
+			$this->components = new \System\ComponentModel\Container();
+			$this->AutoScaleMode = \System\Windows\Forms\AutoScaleMode::Font;
+			$this->SuspendLayout();
+			$this->ClientSize = new \System\Drawing\Size(292, 266);
+			$this->Name = 'Form1';
+			$this->Text = 'Form1';
+			$this->ResumeLayout(false);
+			$this->PerformLayout();
+		}
+		/// <summary>Required designer variable.</summary>
+		private $components = null;
+	}    
+}
+?>
